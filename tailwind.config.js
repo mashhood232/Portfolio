73bed76219239ef185cb29630d0829eb7612008b
Mashhood Shafqat <bscs23188@itu.edu.pk>
2026-01-13 23:24:50 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./**/*.html",
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors:{
+
+      },
+      backgroundColor
+    },
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+}
